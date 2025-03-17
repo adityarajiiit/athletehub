c463aef484a0b1810db36fe3aea2d94db932a816
Aditya Raj <aditya0405raj@gmail.com>
2025-03-17 17:16:25 +0530

    new

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -2,18 +2,18 @@ import jwt from 'jsonwebtoken';
 export const  verifyToken=(req,res,next)=>{
    const token=req.cookies.token;
    if(!token){
-    return res.status(401).json({error:"Unauthorized"});
+    return res.json({error:"Unauthorized"});
     }
     try{
         const decoded=jwt.verify(token,process.env.JWT_SECRET);
         if(!decoded){
-            return res.status(401).json({error:"Unauthorized"});
+            return res.json({error:"Unauthorized"});
         }
         req.userId=decoded.id;
         next()
     }
 
     catch(error){
-return res.status(401).json({error:"Server error",error});
+return res.status(500).json({error:"Server error",error});
     }
 }
