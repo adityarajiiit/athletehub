b0db33563e6c341f63134ac1525c2ba826a9c6ae
Aryanchoudhary12 <aryankumar911315@gmail.com>
2026-07-10 15:22:26 +0530

    correcting network

--- backend/src/lib/email.js
+++ backend/src/lib/email.js
@@ -6,6 +6,7 @@ const transport = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  family: 4,
 });
 
 export const sendVerificationEmail = async (email, verificationToken) => {
